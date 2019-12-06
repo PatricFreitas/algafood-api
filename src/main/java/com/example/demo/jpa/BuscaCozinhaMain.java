c7c31c5c57520d889cac30001a745d8669888b6b
patric.freitas <patric.freitas@smi.net.br>
2019-12-06 18:20:52 -0300

    Aula 4.27

---
+++ src/main/java/com/example/demo/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,26 @@
+package com.example.demo.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.AlgafoodApiApplication;
+import com.example.demo.domain.model.Cozinha;
+import com.example.demo.domain.repository.CozinhaRepository;
+
+public class BuscaCozinhaMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CozinhaRepository cozinhaRepository = applicationContext.getBean(CozinhaRepository.class);
+		
+		Cozinha cozinha = cozinhaRepository.buscar(1L);
+		
+		System.out.println(cozinha.getNome());
+		
+	}
+
+}
