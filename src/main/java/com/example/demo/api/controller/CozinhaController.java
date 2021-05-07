bdb1458c3569f12617ad5e3dda61019648c2c78c
patric.freitas <patric.freitas@smi.net.br>
2021-05-07 19:44:26 -0300

    9.10. Customizando mensagens de validação na anotação da constraint

--- src/main/java/com/example/demo/api/controller/CozinhaController.java
+++ src/main/java/com/example/demo/api/controller/CozinhaController.java
@@ -2,6 +2,8 @@ package com.example.demo.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -47,7 +49,8 @@ public class CozinhaController {
 	}
 	
 	@PutMapping("/{cozinhaId}")
-	public Cozinha atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha){
+	public Cozinha atualizar(@PathVariable Long cozinhaId, 
+			@RequestBody @Valid Cozinha cozinha){
 		
 		Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(cozinhaId);	
 							
